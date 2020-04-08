490253ab554ac484f7552e4b3b0a22d0e6f6cc99
Jimmy Huynh <jkchuynh829@gmail.com>
2020-04-08 14:56:40 -0700

    init with configs and initial files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <div className="contaienr">
+    <h1>Game Show</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
